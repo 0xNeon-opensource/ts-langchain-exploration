d41a9a9f8fb7ef27b34e6032c093e22374417d3d
NΞ◎N <90662438+0xNeon-opensource@users.noreply.github.com>
2023-03-13 20:46:12 -0700

    fix next/font

--- providers/GlobalStylesProvider.tsx
+++ providers/GlobalStylesProvider.tsx
@@ -1,4 +1,4 @@
-import { Open_Sans } from '@next/font/google';
+import { Open_Sans } from "next/font/google";
 import classNames from 'lib/styles/classNames';
 
 export interface GlobalStylesProviderProps
