414c3c13ef63edbaa2f954fa33a2101c08055cd0
NΞ◎N <90662438+0xNeon-opensource@users.noreply.github.com>
2023-04-10 18:27:02 -0700

    show prompt and result, add formatting

--- components/HelloLangChain.tsx
+++ components/HelloLangChain.tsx
@@ -1,22 +1,35 @@
 import { OpenAI } from 'langchain/llms/openai';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 
 interface HelloLangChainProps {}
 
 const HelloLangChain: React.FC<HelloLangChainProps> = () => {
+  const [result, setResult] = useState('');
+
+  const prompt =
+    'What would be a good company name a company that makes colorful socks?';
+
   useEffect(() => {
     const openAIApiKey = process.env.NEXT_PUBLIC_OPENAI_API_KEY;
     const model = new OpenAI({ openAIApiKey, temperature: 0.9 });
     const callModel = async () => {
-      const res = await model.call(
-        'What would be a good company name a company that makes colorful socks?'
-      );
-      console.log(res);
+      const res = await model.call(prompt);
+      setResult(res);
     };
     callModel();
   }, []);
 
-  return <div>Hello, LangChain! 🦜⛓️</div>;
+  return (
+    <div className="border border-blue-400 p-2">
+      <h2>Hello, LangChain! 🦜⛓️</h2>
+      <p>
+        Prompt: <code>{prompt}</code>
+      </p>
+      <p>
+        Result: <code>{result}</code>
+      </p>
+    </div>
+  );
 };
 
 export default HelloLangChain;
